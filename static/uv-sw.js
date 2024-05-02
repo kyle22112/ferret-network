0e7a5639f8f85fa3d3444f0b36e74c7680208e6d
Galactic Network <114846211+GalacticNetwork@users.noreply.github.com>
2024-05-01 20:40:03 -0700

    Update uv-sw.js

--- static/uv-sw.js
+++ static/uv-sw.js
@@ -2,4 +2,4 @@ importScripts("/static/uv/uv.sw.js");
 
 const sw = new UVServiceWorker();
 
-self.addEventListener("fetch", (event) => event.respondWith(sw.fetch(event)));
+self.addEventListener("fetch", (event) => event.respondWith(sw.fetch(event)))
