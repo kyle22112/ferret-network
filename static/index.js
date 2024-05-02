c7c5ac4d7d3c81457f152d2099ece3cb06fca560
Galactic Network <114846211+GalacticNetwork@users.noreply.github.com>
2024-05-01 20:40:59 -0700

    Update index.js

--- static/index.js
+++ static/index.js
@@ -33,4 +33,4 @@ form.addEventListener("submit", async (event) => {
 
   const url = search(address.value, searchEngine.value);
   location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
-});
+})
