94299694baf8770375cbba61d6d0d17b6e8f7717
Galactic Network <114846211+GalacticNetwork@users.noreply.github.com>
2024-09-28 19:52:18 -0700

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/fn/',
-    bare:'https://mathematics-english.vercel.app',
+    bare: 'https://v4-virid-three.vercel.app/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
